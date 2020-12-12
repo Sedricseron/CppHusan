8ca997e744ef025fcdf3b8c05b8aafc4bbe03d1a
Sedricseron <71881493+Sedricseron@users.noreply.github.com>
2020-12-12 12:40:24 +0200

    Husan Dmytro, DataStruct
    Дай Бог, щоб запустилося

---
+++ DataStruct.h
@@ -0,0 +1,21 @@
+﻿// DataStruct.h : включаемый файл для стандартных системных включаемых файлов
+// или включаемые файлы для конкретного проекта.
+
+#pragma once
+
+#include "DLine.h"
+
+template <class data>
+class DataStruct {
+public:
+    virtual bool append(data p) = 0;
+    virtual data pop() = 0;
+    virtual bool is_empty() = 0;
+    virtual bool is_present(data p) = 0;
+    virtual size_t find(data p) = 0;
+    virtual bool remove(data p) = 0;
+    virtual vector<data> find_all(data p1, data p2) = 0;
+};
+
+
+// TODO: установите здесь ссылки на дополнительные заголовки, требующиеся для программы.
